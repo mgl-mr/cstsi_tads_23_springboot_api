a921ad74e2c53daf4732b4a30f0f604fbeab1647
Aluno <aluno@example.com>
2023-10-30 15:18:21 -0300

    refactor: criação do token para test

--- src/test/java/br/edu/ifsul/cstsi_tads_2023/api/jogador/BaseAPITest.java
+++ src/test/java/br/edu/ifsul/cstsi_tads_2023/api/jogador/BaseAPITest.java
@@ -4,6 +4,7 @@ import br.edu.ifsul.cstsi_tads_2023.CstsiTads2023Application;
 import br.edu.ifsul.cstsi_tads_2023.api.infra.security.jwt.JwtUtil;
 import org.junit.Before;
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,6 +31,14 @@ public abstract class BaseAPITest {
 
 
     HttpHeaders getHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken);
+        return headers;
+    }
+
+    @BeforeEach
+    public void setupTest() {
+        System.out.println("setupTest:");
         UserDetails user = userDetailsService.loadUserByUsername("ana@email.com");
         assertNotNull(user);
 
@@ -37,23 +46,7 @@ public abstract class BaseAPITest {
         System.out.println("jwtToken gerado:");
         System.out.println(jwtToken);
         assertNotNull(jwtToken);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken);
-        return headers;
     }
-
-//    @Before
-//    public void setupTest() {
-//        System.out.println("setupTest:");
-//        UserDetails user = userDetailsService.loadUserByUsername("ana@email.com");
-//        assertNotNull(user);
-//
-//        jwtToken = JwtUtil.createToken(user);
-//        System.out.println("jwtToken gerado:");
-//        System.out.println(jwtToken);
-//        assertNotNull(jwtToken);
-//    }
 
     <T> ResponseEntity<T> post(String url, Object body, Class<T> responseType) {
         HttpHeaders headers = getHeaders();
